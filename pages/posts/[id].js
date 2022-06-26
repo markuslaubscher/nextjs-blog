0f5a6a969cecbbb0a4127ef052c1932badf3e371
Markus Laubscher <markuslaubscher@gmail.com>
2022-06-26 12:16:47 +0200

    updated pages/posts/[id].js
    now displays html from markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,7 +11,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -26,5 +26,7 @@ export default function Post({ postData}) {
     {postData.id}
     <br />
     {postData.date}
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>;
 }
